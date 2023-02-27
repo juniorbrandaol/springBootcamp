f8583d50d898d5a29ea86a0b11cb6388b1ce3c6a
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-27 17:07:11 -0300

    Database seeding

--- backend/src/main/java/com/eblj/catalog/servicies/impl/CategoryServiceImpl.java
+++ backend/src/main/java/com/eblj/catalog/servicies/impl/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eblj.catalog.entities.Category;
 import com.eblj.catalog.repositories.CategoryRepository;
@@ -16,6 +17,7 @@ public class CategoryServiceImpl implements CategoryService {
 	private CategoryRepository repository;
 
 	@Override
+	@Transactional(readOnly =true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
